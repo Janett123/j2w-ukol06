294fac03f58eb427edba278342521ae6b89ea2d6
Janett123 <masiko@centrum.cz>
2023-11-20 10:16:26 +0100

    Zobrazení seznamu vizitek z databáze

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -0,0 +1,22 @@
+package cz.czechitas.java2webapps.ukol6.controller;
+
+import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VizitkaController {
+
+    private final VizitkaRepository vizitkaRepository;
+
+    public VizitkaController(VizitkaRepository vizitkaRepository) {
+        this.vizitkaRepository = vizitkaRepository;
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        return new ModelAndView("seznam")
+                .addObject("seznamVizitek", vizitkaRepository.findAll());
+    }
+}
